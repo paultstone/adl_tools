1a55f3ff720d40d5226134e5df1d2eaf789ed4c6
Paul Stone <paul@stone.org>
2024-10-29 08:57:02 -0700

    Only get the specified team's roster

--- team_mgr/team_mgr.R
+++ team_mgr/team_mgr.R
@@ -1,11 +1,12 @@
 #
 # Next steps:
-#  - have a button which:
-#      - removes the first tab, 
-#      - unhides all tabs, and 
-#      - switches to tab 2
-#
-#  - only show the roster for the selected team
+#  - Have two tabsetpanels, one with hidden tabset type
+#    and the other with pills tabset type. The two tabsets
+#    will be wrapped in conditionalPanel, such that only 
+#    one tabSetPanel can be active at a time, and this
+#    is changed by pressing the "Continue" button.
+# 
+#  - Eliminate unneeded fields in the team roster
 #
 
 
@@ -78,46 +79,83 @@ getConferenceRosters <- function(input_conference) {
     adl$adl_rosters(adl_connection, "nfc")
 }
 
+getTeamRoster <- function(input_conference, team_name) {
+  if (input_conference == "AFC") {
+    conf_rosters <- adl$adl_rosters(adl_connection, "afc")
+    team_id <- 
+      adl_afc_franchise_names$franchise_id[
+        adl_afc_franchise_names$franchise_name==team_name]
+    team_roster <- adl_afc_rosters[adl_afc_rosters$franchise_id == team_id,]
+  } else {
+    conf_rosters <- adl$adl_rosters(adl_connection, "nfc")
+    team_id <- 
+      adl_nfc_franchise_names$franchise_id[
+        adl_nfc_franchise_names$franchise_name==team_name]
+    team_roster <- adl_nfc_rosters[adl_nfc_rosters$franchise_id == team_id,]
+  }
+}
+
 ui <- fluidPage(
     # Application title
     #titlePanel("Select your conference and team"),
     #renderText(
     #  {"Select your conference and franchise:"}),
-  tabsetPanel(type="hidden",
-    tabPanel("Select franchise", 
+  tabsetPanel(type="pills", id="FirstPanel",
+    tabPanel("SelectTeam",
       radioButtons("conference_name", "Conference", c("AFC", "NFC")),
       uiOutput('franchises'),
-      actionButton("Team Selected", "Continue")),
-    tabPanel("Summary",
-      uiOutput('summary'))
+      actionButton("TeamSelected", "Continue")),
+    #tabPanel("Summary", type="hidden",
+    #  uiOutput('summary')#)
   )
 )
 
 # Define server logic required to draw a histogram
-server <- function(input, output) {
+server <- function(input, output, session) {
   #conference_name <- reactive(input$conference_name)
 
   output$franchises = renderUI({
-    franchise_list <- 
+    #hideTab(inputId = "FirstPanel", target = "SelectTeam")
+        franchise_list <- 
       if(input$conference_name == "AFC")
         afc_franchises
       else 
         nfc_franchises
-    radioButtons('franchise_name', "Team Name", franchise_list)
+    radioButtons('team_name', "Team Name", franchise_list)
+  })
+  
+  observeEvent(input$TeamSelected, {
+    appendTab(inputId = 
+      "FirstPanel", tab = tabPanel("Summary", uiOutput('summary')))
+    #appendTab(inputId = "FirstPanel", tab = tabPanel("tab3 with a lot of stuff in it",value = "tab3_val", br(), h4("this is tab3")))
+    
+    updateTabsetPanel(session, "FirstPanel",
+                      selected = "Summary")
+    removeTab(inputId = "FirstPanel", target = "SelectTeam")
   })
   
 #  team_id <- 
 #    adl_nfc_franchise_names$franchise_id[
 #      adl_nfc_franchise_names$franchise_name==cur_team]
   
-  output$conference_rosters <- 
+  output$summary_table <- 
     renderDataTable({
-      getConferenceRosters(input$conference_name)
+      team_roster <- 
+        getTeamRoster(input$conference_name, input$team_name)
     })
   
   output$summary <- renderUI({
-       dataTableOutput("conference_rosters")
+       dataTableOutput("summary_table")
   })
+  
+#  output$conference_rosters <- 
+#    renderDataTable({
+#      getConferenceRosters(input$conference_name)
+#    })
+#  
+#  output$summary <- renderUI({
+#    dataTableOutput("conference_rosters")
+#  })
 }
 
 # Run the application 
